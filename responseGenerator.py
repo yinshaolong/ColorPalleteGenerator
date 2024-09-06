dbe8e3dab849198ee929a3bb35f0b16abedbb0e0
omgitsjohn <syin18@my.bcit.ca>
2024-09-06 01:12:47 -0700

    updated model and matching syntax to 4o-mini

--- responseGenerator.py
+++ responseGenerator.py
@@ -1,28 +1,39 @@
-import openai
-from dotenv import dotenv_values
+# import openai
 import json
+from dotenv import load_dotenv
+import openai
+import os
+import argparse
 
-config = dotenv_values(".env")
-openai.api_key = config["OPENAI_KEY"]
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+client = openai.OpenAI()
+
 def get_prompt():
     with open("promptColor.txt") as f:
         return f.read()
     
+
 def response_gen(query):
-    prompt = f"{get_prompt()}{query}\nA: "
-    response = openai.completions.create(
-        prompt=prompt,
-        model="gpt-3.5-turbo-instruct",
-        max_tokens=200,
-        stop="11."
+    # query = {"role" : "user", "'Convert the following verbal description of a color palette into a list of colors": query}
+    # prompt = json.loads(get_prompt())
+    # prompt.append(query)
+    # print(prompt)
+    response = client.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = [
+    {"role": "system", "content": "You are a color palette generating assistant that responds to text prompts for color palettes. You should generate color palettes that fit the theme, mood, or instructions in the prompt. The palettes should be between 2 and 8 colors."},
+    {"role": "user", "content": "Convert the following verbal description of a color palette into a list of colors: The Mediterranean Sea"},
+    {"role": "assistant", "content": '["#006699", "#66CCCC", "#F0E68C", "#008000", "#F08080"]'},
+    {"role": "user", "content": "Convert the following verbal description of a color palette into a list of colors: sage, nature, earth"},
+    {"role": "assistant", "content": '["#EDF1D6", "#9DC08B", "#609966", "#40513B"]'},
+    {"role": "user", "content": "Convert the following verbal description of a color palette into a list of colors:"},
+    {"role" : "user", "content":f'Convert the following verbal description of a color palette into a list of colors: {query}'},
+]
     )
-    # return json.loads(response['choices'[0]['text'])
-
-    #Completion is a dictionary. Choices is a dictionary with a list as an object. The first index of the list is a dictionary with text as a key. 
-    return json.loads(response.choices[0].text)
-
+    return json.loads(response.choices[0].message.content)
 def main():
-    query = input("Enter a color: ")
+    query = input("Enter a prompt to generate a color: ")
     print(response_gen(query))
 if __name__ == "__main__":
-    main()
+     main()
